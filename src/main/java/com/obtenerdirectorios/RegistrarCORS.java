9627f49fadda6c905ef775a6887fcb9e5dabb922
John Hader Medina Pacheco <jhopon94@gmail.com>
2024-05-15 21:26:56 -0500

    Agregado CORS

---
+++ src/main/java/com/obtenerdirectorios/RegistrarCORS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.obtenerdirectorios;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class RegistrarCORS implements Filter{
+   
+   @Override
+   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+        chain.doFilter(req, res);
+    }
+
+   @Override
+    public void init(FilterConfig filterConfig) {}
+
+   @Override
+    public void destroy() {}
+   
+}
